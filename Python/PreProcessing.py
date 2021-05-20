252f706061af3e2ddb0486d6aa494f69ead8c319
DESKTOP-9MNFRV1\User <balazsunio@gmail.com>
2021-05-20 19:18:08 +0200

    Added circle detection feature

--- Python/PreProcessing.py
+++ Python/PreProcessing.py
@@ -2,18 +2,40 @@ import numpy as np
 import cv2
 
 class PreProcessing:
-    def __init__(self, image):
-        self.image = image
+    def __init__(self):
+        pass
 
-    def segment_colors(self, is_show=False):
-        red_image = self.segment_red_color()
-        blue_image= self.segment_blue_color()
-        yellow_image = self.segment_yellow_color()
+    def detect(self, image, is_show=False):
+        red_image, blue_image, yellow_image = self.segment_colors(image, is_show=is_show)
+        self.detecct_circle(red_image, is_show=is_show, test_image=image)
+
+    def segment_colors(self, image, is_show=False):
+        red_image = self.segment_red_color(image)
+        blue_image= self.segment_blue_color(image)
+        yellow_image = self.segment_yellow_color(image)
         if is_show:
             self.imshow("red mask", red_image)
             self.imshow("blue mask", blue_image)
             self.imshow("yellow mask", yellow_image)
 
+        return red_image, blue_image, yellow_image
+
+    def detecct_circle(self, image, is_show=False, test_image=np.empty((1,1))):
+        minDist = 100
+        param1 = 500  # 500
+        param2 = 20  # 200 #smaller value-> more false circles
+        minRadius = 5
+        maxRadius = 100  # 10
+
+        circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2,
+                                   minRadius=minRadius, maxRadius=maxRadius)
+
+        if is_show:
+            if circles is not None:
+                circles = np.uint16(np.around(circles))
+                for i in circles[0, :]:
+                    cv2.circle(test_image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                self.imshow("detected circles", test_image)
 
 
     def segment_red_color(self, image):
--- Python/main.py
+++ Python/main.py
@@ -7,34 +7,33 @@ import cv2
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def test_image():
+    image=cv2.imread("Test/maps_hungaria.png")
+    pre_processing=PreProcessing.PreProcessing()
+    pre_processing.detect(image, is_show=True)
 
-
-def print_hi(name):
-
+def test_video():
     """
     pre_processing.segment_colors()
     #pre_processing.detect_circle(pre_processing.image)
     """
-    inputHandler = MyInputHandler.InputHandler()
-    pre_processing = PreProcessing.PreProcessing(inputHandler.image)
+    pre_processing = PreProcessing.PreProcessing()
     cap = cv2.VideoCapture("Test/video.mp4")
 
     while cap.isOpened():
         t = time.time()
         ret, frame = cap.read()
-        pre_processing.image=frame
-        pre_processing.segment_colors()
+        pre_processing.detect(frame)
         print(time.time() - t)
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) == ord('q'):
             break
 
     print(time.time()-t)
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    test_video()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
